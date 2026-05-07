9057cd05b4af070f782ffa2779e04553826058f3
Abhi@1997Git <abhimanna007@gmail.com>
2026-05-07 23:47:15 +0530

    module1 basic changes of springboot

---
+++ src/main/java/com/cs/codingshuttleone/CodingshuttleoneApplication.java
@@ -0,0 +1,13 @@
+package com.cs.codingshuttleone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingshuttleoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingshuttleoneApplication.class, args);
+	}
+
+}
